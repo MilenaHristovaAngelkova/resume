acc58b5aae03730612a8ff70afe595a3b16c6e18
MilenaHristovaAngelkova <light_milena@abv.bg>
2021-06-22 14:00:25 +0300

    update 1.2

--- static/scripts.js
+++ static/scripts.js
@@ -1,1 +1,33 @@
-// funtion that goes through each section
+function changeContent(el) {
+    let articles = document.querySelectorAll("article");
+    for (let el of articles) {
+        el.setAttribute("style", "display: none");
+    }
+    switch(el.id) {
+        case "menu":
+            document.querySelector(".myPic").setAttribute("style", "display: block");
+            break;
+        case "menuTech": 
+            document.querySelector(".coding").setAttribute("style", "display: block");
+            break;
+        case "menuProjects": 
+            document.querySelector(".projects").setAttribute("style", "display: block");
+            break;
+        case "menuWork": 
+            document.querySelector(".experience").setAttribute("style", "display: block");
+            break;
+        case "menuEdu": 
+            document.querySelector(".education").setAttribute("style", "display: block");
+            break;
+        case "menuSkills": 
+            document.querySelector(".skills").setAttribute("style", "display: block");
+            break;
+        case "menuContact":
+            document.querySelector(".contactInfo").setAttribute("style", "display: block");
+            break;
+    }
+}
+
+// function showWorkMenu() {
+//     document.querySelector(".workMenu").
+// }
